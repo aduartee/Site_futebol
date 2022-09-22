36aff8ebc304a11d72fb1f2993c530966d8b201e
Arthur <arthurduartealves99@gmail.com>
2022-09-22 20:55:09 -0300

    Adicionando filtros no /admin

--- futebol/admin.py
+++ futebol/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Futebol
 
-
-admin.site.register(Futebol)
+class Listando_times(admin.ModelAdmin):
+    list_display = ('id', 'nome_time', 'numero_titulos')
+    list_display_links = ('id', 'nome_time')
+    search_fields = ('nome_time',)
+    list_per_page = 5
+    
+    
+admin.site.register(Futebol, Listando_times)
