5b1e71515305697540b99dabc96c1bee93949bf9
Arthur <arthurduartealves99@gmail.com>
2022-09-22 21:08:59 -0300

    Registrando o app de pessoas no /admin

--- pessoas/models.py
+++ pessoas/models.py
@@ -1,3 +1,6 @@
+import email
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=150)
+    email = models.CharField(max_length=200)
