3847fc77b1ded20f1eebf5296dffea5eb97abe60
Arthur <arthurduartealves99@gmail.com>
2022-09-27 22:19:33 -0300

    Adicionado imagens de na receita e na pagina da receita

--- site_futebol/urls.py
+++ site_futebol/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from site_futebol.settings import STATICFILES_DIRS
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('futebol.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
