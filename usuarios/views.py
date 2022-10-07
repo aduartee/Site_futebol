2270fe357fc7bc9e84fff398410c0990b3da0bb5
Arthur <arthurduartealves99@gmail.com>
2022-10-06 22:44:33 -0300

    Adicionando função de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import user
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        name = request.POST('nome')
+        email = request.POST('email')
+        senha = request.POST('password')
+        senha2 = request.POST('password2')
+        if not name.strip():
+            print('Nome não pode ficar em branco')
+            return redirect('cadastro')
+        
+        if not email.strip('Email não pode fica vazio'):
+            return redirect('cadastro')
+        
+        if senha != senha2: 
+            print('Senhas devem ser iguais')
+            return redirect('cadastro')
+        
+        if 
+        print('Usuario cadastrado com sucesso: ', name, email, senha, senha2 )
+        
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
+
 
 def login(request):
     return render(request, 'usuarios/login.html')
